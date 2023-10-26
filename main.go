c9933cfc01b41a6bbaba2ccb635f25d12ea186ec
Kzxky1 <auxsoftware19@gmail.com>
2023-10-26 13:50:39 +0530

    the color changed

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ type album struct {
 }
 
 var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+    {ID: "1", Title: "Magenta Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Teal", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford yellow", Artist: "Sarah Vaughan", Price: 39.99},
 }
   
 func main() {
